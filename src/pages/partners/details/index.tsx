c34bba37a326824d3761e2becdbe51aa92ac0f64
Davidxing1 <937104001@qq.com>
2022-01-06 21:05:15 +0800

    test更新组件 1/6 -2

--- src/pages/partners/details/index.tsx
+++ src/pages/partners/details/index.tsx
@@ -102,6 +102,7 @@ export default function Details() {
     const cancelButtonRef = useRef(null)
     const [openno, setOpenno] = useState(false)
     const [open, setOpen] = useState(false)
+    const[openload ,setOpenload]=useState(true)
     async function Pay() {
         setOpenno(false)
         const web3Enable = (await import("@polkadot/extension-dapp")).web3Enable;
@@ -130,13 +131,16 @@ export default function Details() {
         transferExtrinsic.signAndSend(account.address, {signer: injector.signer}, ({status}) => {
             if (status.isInBlock) {
                 console.log(`Completed at block hash #${status.asInBlock.toString()}`);
+                setOpenload(true)
+            } else if (status.isFinalized) {
+                setOpenload(false)
                 setOpen(true)
-            } else {
+            }else{
                 console.log(`Current status: ${status.type}`);
-                setOpenno(true)
             }
         }).catch((error: any) => {
             console.log(':( transaction failed', error);
+            setOpenload(false)
             setOpenno(true)
         });
     }
@@ -149,6 +153,7 @@ export default function Details() {
                     <Top></Top>
                     <div>
                         <Left></Left>
+
                         <div className="md:ml-5 mt-8">
                             <div className="bg-white">
                                 <div className="mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -387,7 +392,7 @@ export default function Details() {
                 </div>
             </div>
             <Transition.Root show={open} as={Fragment}>
-                <Dialog as="div" className="fixed z-20 inset-0 overflow-y-auto" onClose={setOpen}>
+                <Dialog as="div" className="fixed z-20 inset-0 overflow-y-auto " onClose={setOpen}>
                     <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                         <Transition.Child
                             as={Fragment}
@@ -416,10 +421,10 @@ export default function Details() {
                             <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                                 <div>
                                     <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-                                        <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                                        <CheckIcon className="h-6 w-6  text-green-600" aria-hidden="true" />
                                     </div>
                                     <div className="mt-3 text-center sm:mt-5">
-                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                        <Dialog.Title as="h3" className=" text-lg leading-6 font-medium text-gray-900">
                                             Payment successful
                                         </Dialog.Title>
                                         <div className="mt-2">
@@ -502,6 +507,55 @@ export default function Details() {
                                     >
                                         Cancel
                                     </button>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+
+            <Transition.Root show={openload} as={Fragment}>
+                <Dialog as="div" className="fixed z-20 inset-0 overflow-y-auto " onClose={setOpenload}>
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;
+          </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+                                <div>
+                                    <div className="mx-auto flex items-center justify-center h-12 animate-spin w-12 text-5xl ">
+                                        <i className="fa fa-spinner" aria-hidden="true"></i>
+
+                                    </div>
+                                    <div className="mt-3 text-center sm:mt-5">
+                                        <Dialog.Title as="h3" className=" text-lg leading-6 font-medium text-gray-900">
+                                          Loading.......
+                                        </Dialog.Title>
+                                        <div className="mt-2">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mt-5 sm:mt-6">
                                 </div>
                             </div>
                         </Transition.Child>
